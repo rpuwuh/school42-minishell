af53a35c6676f6ac69ff3e420caf10310c0cf32a
Beedrill Poetess <bpoetess@at-q6.msk.21-school.ru>
2022-08-29 16:59:35 +0300

    heredoc changed

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:19:30 by bpoetess          #+#    #+#             */
-/*   Updated: 2022/08/29 16:55:07 by bpoetess         ###   ########.fr       */
+/*   Updated: 2022/08/29 16:59:22 by bpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ int	here_doc_fd(char *stop_word)
 	int		fds[2];
 	char	tmp;
 
-	if (!stop_word)
-		return (0);
-	if (pipe(fds) == -1)
+	if (!stop_word || pipe(fds) == -1)
 		return (0);
 	tmp = readline(YELLOW"> "RESETCOLOR);
 	while (tmp && !ft_strncmp(tmp, stop_word, strlen(tmp) - 1)
