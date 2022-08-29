2899226d127a74c57e604b63f4265bc5e2205d7a
Beedrill Poetess <bpoetess@at-q6.msk.21-school.ru>
2022-08-29 16:40:42 +0300

    new_heredoc_added

---
+++ src/here_doc.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_doc.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/29 16:19:30 by bpoetess          #+#    #+#             */
+/*   Updated: 2022/08/29 16:38:35 by bpoetess         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+int	here_doc(char *stop_word)
+{
+	int		fds[2];
+	char	tmp;
+
+	if (!stop_word)
+		return (0);
+	if (pipe(fds) == -1)
+		return (0);
+	tmp = readline(YELLOW"> "RESETCOLOR);
+	while (tmp && !ft_strncmp(tmp, stop_word, strlen(tmp) - 1)
+		&& ft_strncmp(tmp, stop_word, strlen(tmp)) == '\n')
+	{
+		write(fds[1], tmp, ft_strlen(tmp));
+		free (tmp);
+		tmp = 0;
+		tmp = readline(YELLOW"> "RESETCOLOR);
+	}
+	if (tmp)
+		free (tmp);
+	close (fds[1]);
+	return (fds[0]);
+}
