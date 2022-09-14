c0597d50e01d69e635ac137a304912ac8bc0bf18
Mikhail <sir.mishka828@gmail.com>
2022-09-14 15:07:50 +0300

    bpoetess's here_doc fix

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:19:30 by bpoetess          #+#    #+#             */
-/*   Updated: 2022/08/29 16:59:22 by bpoetess         ###   ########.fr       */
+/*   Updated: 2022/09/14 15:07:09 by bpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	here_doc_fd(char *stop_word)
 	int		fds[2];
 	char	tmp;
 
-	if (!stop_word || pipe(fds) == -1)
-		return (0);
+	if (!stop_word || pipe(fds) < 0)
+		return (-1);
 	tmp = readline(YELLOW"> "RESETCOLOR);
-	while (tmp && !ft_strncmp(tmp, stop_word, strlen(tmp) - 1)
-		&& ft_strncmp(tmp, stop_word, strlen(tmp)) == '\n')
+	while (tmp && !ft_strncmp(tmp, stop_word, strlen(tmp)))
 	{
+		write(fds[1], "\n", 1);
 		write(fds[1], tmp, ft_strlen(tmp));
 		free (tmp);
 		tmp = 0;
