acef950b29c660a896d613c37c8cbb246fce1c49
Mikhail <sir.mishka828@gmail.com>
2022-09-16 17:01:08 +0300

    here_doc fix

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,11 +6,21 @@
 /*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:19:30 by bpoetess          #+#    #+#             */
-/*   Updated: 2022/09/16 15:45:45 by bpoetess         ###   ########.fr       */
+/*   Updated: 2022/09/16 16:39:54 by bpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+static void	stop_prompt(int sig)
+{
+	g_status = 130;
+	write(1, "\n", 1);
+	rl_replace_line("", 0);
+	// rl_on_new_line();
+	// rl_redisplay();
+	(void)sig;
+}
 
 int	here_doc_fd(char *stop_word)
 {
@@ -19,15 +29,12 @@ int	here_doc_fd(char *stop_word)
 
 	if (!stop_word || !*stop_word || pipe(fds) < 0)
 		return (-1);
+	ft_signals_run(3);
+	signal(SIGINT, stop_prompt);
 	tmp = readline(YELLOW"> "RESETCOLOR);
-	ft_signals_run(4);
-	printf ("\n");
-	printf("\tstr = <%s>\n", tmp);
-	while (g_status != 130 && ((!tmp || !*tmp)
+	while (g_status != 130 && ((tmp && !*tmp)
 			|| (tmp && ft_strncmp(tmp, stop_word, strlen(tmp)))))
 	{
-		printf("\tstr = <%s>\n", tmp);
-		ft_signals_run(4);
 		write(fds[1], "\n", 1);
 		write(fds[1], tmp, ft_strlen(tmp));
 		free (tmp);
@@ -43,3 +50,24 @@ int	here_doc_fd(char *stop_word)
 	close (fds[1]);
 	return (fds[0]);
 }
+
+/*
+while ((g_status != 130) && (tmp
+			&& ft_strncmp(tmp, stop_word, strlen(tmp))))
+	{
+		ft_signals_run(4);
+		write(fds[1], "\n", 1);
+		write(fds[1], tmp, ft_strlen(tmp));
+		free (tmp);
+		tmp = 0;
+		tmp = readline(YELLOW"> "RESETCOLOR);
+	}
+	if (tmp)
+		free (tmp);
+	if (g_status != 130)
+		write(fds[1], "\n", 1);
+	else
+		g_status = 0;
+	close (fds[1]);
+	return (fds[0]);
+*/
