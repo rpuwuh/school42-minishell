be005820f8f547c0b4780d91b9a944612e192829
Mikhail <sir.mishka828@gmail.com>
2022-09-16 15:16:12 +0300

    here_doc new version

--- src/here_doc.c
+++ src/here_doc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmillan <dmillan@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:19:30 by bpoetess          #+#    #+#             */
-/*   Updated: 2022/09/15 00:19:36 by dmillan          ###   ########.fr       */
+/*   Updated: 2022/09/16 15:15:53 by bpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ int	here_doc_fd(char *stop_word)
 	int		fds[2];
 	char	*tmp;
 
-	if (!stop_word || pipe(fds) < 0)
+	if (!stop_word || !*stop_word || pipe(fds) < 0)
 		return (-1);
 	tmp = readline(YELLOW"> "RESETCOLOR);
 	ft_signals_run(4);
-	while (tmp && !ft_strncmp(tmp, stop_word, strlen(tmp)))
+	while ((g_status != 130) && (tmp
+			&& ft_strncmp(tmp, stop_word, strlen(tmp))))
 	{
 		ft_signals_run(4);
 		write(fds[1], "\n", 1);
@@ -32,6 +33,10 @@ int	here_doc_fd(char *stop_word)
 	}
 	if (tmp)
 		free (tmp);
+	if (g_status != 130)
+		write(fds[1], "\n", 1);
+	else
+		g_status = 0;
 	close (fds[1]);
 	return (fds[0]);
 }
