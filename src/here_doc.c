7c6bd05f515e93559ba232846c7de45523098b49
Mikhail <sir.mishka828@gmail.com>
2022-09-16 17:32:18 +0300

    here_doc ctrl+D fix

--- src/here_doc.c
+++ src/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: bpoetess <bpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:19:30 by bpoetess          #+#    #+#             */
-/*   Updated: 2022/09/16 16:39:54 by bpoetess         ###   ########.fr       */
+/*   Updated: 2022/09/16 17:31:18 by bpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,31 +22,40 @@ static void	stop_prompt(int sig)
 	(void)sig;
 }
 
-int	here_doc_fd(char *stop_word)
+static void	here_doc_loop(char *stop_word, int fd)
 {
-	int		fds[2];
 	char	*tmp;
+	int		first_line;
 
-	if (!stop_word || !*stop_word || pipe(fds) < 0)
-		return (-1);
-	ft_signals_run(3);
-	signal(SIGINT, stop_prompt);
+	first_line = 1;
 	tmp = readline(YELLOW"> "RESETCOLOR);
 	while (g_status != 130 && ((tmp && !*tmp)
 			|| (tmp && ft_strncmp(tmp, stop_word, strlen(tmp)))))
 	{
-		write(fds[1], "\n", 1);
-		write(fds[1], tmp, ft_strlen(tmp));
+		if (!first_line)
+			write(fd, "\n", 1);
+		write(fd, tmp, ft_strlen(tmp));
 		free (tmp);
+		first_line = 0;
 		tmp = 0;
 		tmp = readline(YELLOW"> "RESETCOLOR);
 	}
 	if (tmp)
 		free (tmp);
-	if (g_status != 130)
-		write(fds[1], "\n", 1);
-	else
-		g_status = 0;
+	if (g_status == 130)
+		write(fd, "\n", 1);
+	g_status = 0;
+}
+
+int	here_doc_fd(char *stop_word)
+{
+	int		fds[2];
+
+	if (!stop_word || !*stop_word || pipe(fds) < 0)
+		return (-1);
+	ft_signals_run(3);
+	signal(SIGINT, stop_prompt);
+	here_doc_loop(stop_word, fds[1]);
 	close (fds[1]);
 	return (fds[0]);
 }
